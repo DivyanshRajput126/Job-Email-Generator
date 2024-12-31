f16afb9cbc66b6d3e48d57237321bacb01f942a5
DivyanshRajput126 <divyanshrajput126@gmail.com>
2024-12-31 19:18:55 +0530

    persistent client to client

--- portfolio.py
+++ portfolio.py
@@ -8,7 +8,7 @@ class Portfolio:
     def __init__(self,file_path = "./resource/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.PersistentClient('vector_store')
+        self.chroma_client = chromadb.Client('vector_store')
         self.collection = self.chroma_client.get_or_create_collection(name='portfolio')
         
     def load_portfolio(self):
